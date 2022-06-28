5b53cffd1356b7294e8037185c767d4e4002895e
michidevall <glitzpixy1@yahoo.com>
2022-06-27 23:51:12 -0600

    first commit

---
+++ github.py
@@ -0,0 +1,3 @@
+import requests
+
+CLIENT_ID = 
---
+++ practice.py
@@ -0,0 +1,44 @@
+
+# variable practice
+# message = "hello world"
+# print(message)
+
+# message = "hello python crash course world."
+# print(message)
+
+# names = "Emma, Lance, and Bob."
+# print(names)
+
+# strings
+
+# name = "ada lovelace"
+# print(name.title())
+
+# print(name.upper())
+# print(name.lower())
+
+# f strings
+
+# first_name = "ada"
+# last_name = "lovelace"
+# full_name = f"{first_name} {last_name}"
+# print(full_name)
+# print(full_name.upper())
+
+# first_name = "ada"
+# last_name = "lovelace"
+# full_name = f"{first_name} {last_name}"
+# print(f"Hello, {first_name.title()}!")
+# message = f"hello, {full_name.title()}!"
+# print(message)
+
+# first_name = "Eric"
+# last_name = "Agnew"
+# message = f"Hello {first_name}, would like to learn python today?"
+# print(message)
+# message = f"Is {first_name} super annoying?"
+# print(message)
+# message = f"yes."
+# print(message)
+# message = f"Alber Einstein once said, 'A person who never made a mistake never tried anything new.'"
+# print(message)
